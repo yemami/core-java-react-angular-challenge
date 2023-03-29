492c1293a8ac966a765091cd60e657ae505e7f15
your name <simegnew62@gmail.com>
2023-03-28 19:04:24 -0500

    hello world

---
+++ Challenges for coming a month/4 Reading Core java/coding part start from zero/day one/helloworld/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    static String result = "hello world this is my first code on this challenge ";
+    public static void main(String[] args) {
+        System.out.println(Main.result);
+    }
+}
